7211c804a229813871764f6b12dd1ebb5414e521
rubinnum <romarypiak@gmail.com>
2025-01-06 18:11:22 +0100

    feat: implement opening login activity

--- app/src/main/java/com/example/finalproject/RegistrationActivity.kt
+++ app/src/main/java/com/example/finalproject/RegistrationActivity.kt
@@ -1,11 +1,19 @@
 package com.example.finalproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registration_activity)
+
+        val login = findViewById<TextView>(R.id.login)
+        login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
