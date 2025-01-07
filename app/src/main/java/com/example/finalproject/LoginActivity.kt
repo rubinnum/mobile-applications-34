2bef43c474c77b4cca9ab95150de9a4af0430f7a
rubinnum <romarypiak@gmail.com>
2025-01-08 00:34:57 +0100

    chore: add properties with custom getters

--- app/src/main/java/com/example/finalproject/LoginActivity.kt
+++ app/src/main/java/com/example/finalproject/LoginActivity.kt
@@ -12,6 +12,26 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    private val credentialsManager = CredentialsManager()
+
+    private val registerNow: TextView
+        get() = findViewById(R.id.register_now)
+
+    private val emailLayout: TextInputLayout
+        get() = findViewById(R.id.email_layout)
+
+    private val emailEditText: TextInputEditText
+        get() = findViewById(R.id.email)
+
+    private val passwordLayout: TextInputLayout
+        get() = findViewById(R.id.password_layout)
+
+    private val passwordEditText: TextInputEditText
+        get() = findViewById(R.id.password)
+
+    private val nextButton: Button
+        get() = findViewById(R.id.next_button)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,24 +42,18 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
-        val registerNow = findViewById<TextView>(R.id.register_now)
         registerNow.setOnClickListener {
             displayNewActivity(RegistrationActivity::class.java)
         }
-
-        val credentialsManager = CredentialsManager()
-        val emailLayout = findViewById<TextInputLayout>(R.id.email_layout)
-        val emailEditText = findViewById<TextInputEditText>(R.id.email)
-        val passwordLayout = findViewById<TextInputLayout>(R.id.password_layout)
-        val passwordEditText = findViewById<TextInputEditText>(R.id.password)
-        val nextButton = findViewById<Button>(R.id.next_button)
 
         nextButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
 
-            emailLayout.error = if (!credentialsManager.isEmailValid(email)) getString(R.string.invalid_email_message) else null
-            passwordLayout.error = if (!credentialsManager.isPasswordValid(password)) getString(R.string.invalid_password_message) else null
+            emailLayout.error =
+                if (!credentialsManager.isEmailValid(email)) getString(R.string.invalid_email_message) else null
+            passwordLayout.error =
+                if (!credentialsManager.isPasswordValid(password)) getString(R.string.invalid_password_message) else null
 
             if (credentialsManager.userExists(email, password)) {
                 displayNewActivity(MainActivity::class.java)
