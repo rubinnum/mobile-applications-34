125740ebd11f383857b902e2bc36a775918a29e8
rubinnum <romarypiak@gmail.com>
2025-01-07 23:51:23 +0100

    chore: make main activity to be temporarily empty

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.finalproject
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {}
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
