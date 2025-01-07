d72cfa24ab437e60c1cef8bee59fc5b70b129fe3
rubinnum <romarypiak@gmail.com>
2025-01-07 01:16:19 +0100

    fix: modify email validation method

--- app/src/main/java/com/example/finalproject/CredentialsManager.kt
+++ app/src/main/java/com/example/finalproject/CredentialsManager.kt
@@ -1,8 +1,10 @@
 package com.example.finalproject
 
+import androidx.core.util.PatternsCompat
+
 class CredentialsManager {
     fun isEmailValid(email: String): Boolean =
-        android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()
 
     fun isPasswordValid(password: String): Boolean = password.length >= 8
 }
