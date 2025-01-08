457a79de025b690fc59448b5d747906cb8168bf5
rubinnum <romarypiak@gmail.com>
2025-01-08 01:18:30 +0100

    feat: implement register user method

--- app/src/main/java/com/example/finalproject/CredentialsManager.kt
+++ app/src/main/java/com/example/finalproject/CredentialsManager.kt
@@ -3,6 +3,8 @@ package com.example.finalproject
 import androidx.core.util.PatternsCompat
 
 class CredentialsManager {
+    private var registeredUsers: MutableMap<String, String> = mutableMapOf()
+
     fun isEmailValid(email: String): Boolean =
         PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()
 
@@ -10,4 +12,17 @@ class CredentialsManager {
 
     fun userExists(email: String, password: String): Boolean =
         email == "test@te.st" && password == "12345678"
+
+    fun registerUser(email: String, password: String) {
+        if (!areCredentialsValid(email, password))
+            throw IllegalArgumentException("Invalid credentials")
+
+        if (registeredUsers.containsKey(email))
+            throw IllegalArgumentException("User already exists")
+
+        registeredUsers[email] = password
+    }
+
+    private fun areCredentialsValid(email: String, password: String): Boolean =
+        isEmailValid(email) && isPasswordValid(password)
 }
