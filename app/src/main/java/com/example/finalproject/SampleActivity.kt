228e9b5e976f2c4ea442811a3796b75f7402fefa
rubinnum <romarypiak@gmail.com>
2025-01-18 20:25:13 +0100

    feat: change the fragments when the button is pressed

--- app/src/main/java/com/example/finalproject/SampleActivity.kt
+++ app/src/main/java/com/example/finalproject/SampleActivity.kt
@@ -1,11 +1,38 @@
 package com.example.finalproject
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SampleActivity : AppCompatActivity() {
+    private val changeFragmentButton: TextView
+        get() = findViewById(R.id.change_fragment)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sample_activity)
+
+        val fragmentA = FragmentA()
+        val fragmentB = FragmentB()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragmentA)
+            commit()
+        }
+
+        changeFragmentButton.setOnClickListener {
+            if (fragmentA.isVisible) {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.fragment_container, fragmentB)
+                    commit()
+                    addToBackStack(null);
+                }
+            } else {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.fragment_container, fragmentA)
+                    commit()
+                }
+            }
+        }
     }
 }
